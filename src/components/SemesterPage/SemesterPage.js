ebaa68dfc4d76c58d248a71d2fac15e159e52b26
Yuvaraj643 <vyuvarajnani2001@gmail.com>
2023-05-12 00:36:30 +0530

    now

--- src/components/SemesterPage/SemesterPage.js
+++ src/components/SemesterPage/SemesterPage.js
@@ -1,18 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import axios from "axios";
 
 const SemesterPage = () => {
   const { departmentId } = useParams();
   const [semesters, setSemesters] = useState([]);
 
-  // useEffect(() => {
-  //   const fetchSemesters = async () => {
-  //     const result = await axios('http://localhost:3000/departments/1/semesters');
-  //     setSemesters(result.data);
-  //   };
-  //   fetchSemesters();
-  // }, []);
   useEffect(() => {
     axios
       .get(
@@ -24,22 +17,24 @@ const SemesterPage = () => {
   }, [departmentId]);
 
   return (
-    <div class="container">
+    <>
+      <h2 className="text-center mb-4">Explore All Departments</h2>
       <div class="row row-cols-1 row-cols-md-2 g-3">
-        {departments.map((department) => (
-          <div class="col" key={department.id}>
-            <div class="card">
-              {/* <img src="..." class="card-img-top" alt="..." /> */}
+        <div class="col">
+          {semesters.map((semester) => (
+            <div class="card" key={semester.id}>
               <div class="card-body">
-                <Link to={`/departments/${department.id}/semesters`}>
-                  <h5 class="card-title">{department.name}</h5>
+                <Link
+                  to={`/departments/${departmentId}/semesters/${semester.id}/subjects`}
+                >
+                  <h5 class="card-title">{semester.name}</h5>
                 </Link>
               </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 export default SemesterPage;

d061bccd5f30afaf14ba155cc5e2e9e98821d188
Yuvaraj643 <vyuvarajnani2001@gmail.com>
2023-05-12 23:23:35 +0530

    now

--- src/components/SemesterPage/SemesterPage.js
+++ src/components/SemesterPage/SemesterPage.js
@@ -1,40 +1,43 @@
 import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import axios from "axios";
+import "./style.css";
 
 const SemesterPage = () => {
   const { departmentId } = useParams();
   const [semesters, setSemesters] = useState([]);
 
   useEffect(() => {
-    axios
+    const response = axios
       .get(
         `https://exam-slayer.onrender.com/departments/${departmentId}/semesters`
       )
       .then((response) => {
         setSemesters(response.data);
       });
+    console.log(response.data);
   }, [departmentId]);
 
   return (
     <>
-      <h2 className="text-center mb-4">Explore All Departments</h2>
-      <div class="row row-cols-1 row-cols-md-2 g-3">
-        <div class="col">
-          {semesters.map((semester) => (
-            <div class="card" key={semester.id}>
-              <img src={semester.logo} class="card-img-top" alt="Card image" />
+      <h2 className="text-center mb-4 ">Explore All Departments</h2>
+
+      <div class="row row-cols-1 row-cols-md-4 g-4">
+        {semesters.map((semester) => (
+          <div class="col">
+            <div class="card text-dark bg-light mb-3">
+              <img src={semester.logo} class="card-img-top" alt="image" />
               <div class="card-body">
                 <Link
                   to={`/departments/${departmentId}/semesters/${semester.id}/subjects`}
                 >
-                  <h5 class="card-title">{semester.name}</h5>
+                  <h5 class="card-title text-center">{semester.name}</h5>
                 </Link>
               </div>
             </div>
-          ))}
-        </div>
-      </div>
+          </div>
+        ))}
+      </div>  
     </>
   );
 };

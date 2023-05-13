04fe8cc075d47ade0699315ad5b54aa6b651d8a6
Yuvaraj643 <vyuvarajnani2001@gmail.com>
2023-05-13 17:46:49 +0530

    now

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import DepartmentPage from "./components/DepartmentPage/DepartmentPage";
+// import DepartmentPage from "./components/DepartmentPage/DepartmentPage";
 import SemesterPage from "./components/SemesterPage/SemesterPage";
 import SubjectPage from "./components/SubjectPage/SubjectPage";
 import ChapterPage from "./components/ChapterPage/ChapterPage";
 import Navbar from "./components/Navbar/Navbar"
 import Homepage from "./components/Homapage/HomePage";
-import Footer from "./components/Footer/Footer";
+// import Footer from "./components/Footer/Footer";
 const App = () => {
   return (
     <Router>
       <div className="App">
         <Navbar />
-        <Footer />
+        {/* <Footer /> */}
         <Routes>
         <Route path="/" element={<Homepage />} />
-          <Route path="/departments" element={<DepartmentPage />} />
+          {/* <Route path="/departments" element={<DepartmentPage />} /> */}
           <Route
             path="/departments/:departmentId/semesters"
             element={<SemesterPage />}
--- src/components/Homapage/HomePage.jsx
+++ src/components/Homapage/HomePage.jsx
@@ -1,45 +1,83 @@
 import React from "react";
 import { Button } from "react-bootstrap";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
-import "./style.css"
+import "./style.css";
 function HomePage() {
+  const [departments, setDepartments] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://exam-slayer.onrender.com/departments")
+      .then((response) => {
+        setDepartments(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
   return (
-    <div className="container-fluid">
-      <div className="row align-items-center">
-        <div className="col-md-6 order-md-2">
-        <div className="homepage-image">
-            <img
-              src="https://source.unsplash.com/random/?study,education"
-              alt="Books Studying"
-              className="img-fluid"
-            />
+    <>
+      <div className="container-fluid">
+        <div className="row align-items-center">
+          <div className="col-md-6 order-md-2">
+            <div className="homepage-image">
+              <img
+                src="https://source.unsplash.com/random/800x600/?study,education"
+                alt="Books Studying"
+                className="img-fluid"
+              />
+            </div>
           </div>
-        </div>
-        <div className="col-md-6 order-md-1">
-          <div className="text-center">
-            <h1 className="display-4 fw-bold">ExamSlayer</h1>
-            <p className="lead mb-4">
-              ExamSlayer is an online platform that provides comprehensive study
-              materials for various exams. We aim to help students achieve their
-              academic goals by offering high-quality resources and personalized
-              learning experiences.
-            </p>
-            <div className="d-grid gap-2 col-6 mx-auto">
-              <Link to="./departments">
-                <Button variant="primary" size="lg">
-                  Explore 
-                </Button>
-              </Link>
-              <Link to="./">
-                <Button variant="outline-primary" size="lg">
-                  About us
-                </Button>
-              </Link>
+          <div className="col-md-6 order-md-1">
+            <div className="text-center">
+              {/* <h1 className="display-4 fw-bold">ExamSlayer</h1> */}
+              <p className="lead mb-4">
+                ExamSlayer is an online platform that provides comprehensive
+                study materials for various exams. We aim to help students
+                achieve their academic goals by offering high-quality resources
+                and personalized learning experiences.
+              </p>
+              <div className="d-grid gap-2 col-6 mx-auto">
+                <a href="#container">
+                  <Button variant="primary" size="lg">
+                    Explore
+                  </Button>
+                </a>
+                <Link to="./">
+                  <Button variant="outline-primary" size="lg">
+                    About us
+                  </Button>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+      <section id="container">
+        <h2 className="text-center mb-4">Explore All Departments</h2>
+        <div class="row row-cols-1 row-cols-md-1 g-3">
+          <div class="col">
+            {departments.map((department) => (
+              <Link to={`/departments/${department.id}/semesters`}>
+                <div class="card" key={department.id}>
+                  <div class="card-body">
+                    <img
+                      src={department.logo}
+                      class="card-img-top"
+                      alt="logo"
+                    />
+
+                    <h5 class="card-title">{department.name}</h5>
+                  </div>
+                </div>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
 

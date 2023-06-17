d7200bb102065353cb83d4c1fdc76a6f66d3e658
Yuvaraj643 <vyuvarajnani2001@gmail.com>
2023-06-17 08:47:43 +0530

    now

--- src/components/Sem-Imp/NLP/NLP.js
+++ src/components/Sem-Imp/NLP/NLP.js
@@ -1,20 +1,52 @@
 import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import First from "./First";
+import Second from "./Second";
 
 const NLP = () => {
+  const [activeSection, setActiveSection] = useState("first");
+
+  function showSection(sectionName) {
+    setActiveSection(sectionName);
+  }
   return (
     <>
       <div class="container">
         <div class="text-center">
           <h1 class="display-4">NLP Important Topics</h1>
-          <a href="#first" class="d-block mt-3">
-            <div class="fs-4">First Unit</div>
-          </a>
+          <div class="d-flex justify-content-center mt-3">
+            <a
+              href="#"
+              class="btn btn-primary me-3"
+              onClick={() => showSection("first")}
+            >
+              First Unit
+            </a>
+            <a
+              href="#"
+              class="btn btn-primary"
+              onClick={() => showSection("second")}
+            >
+              Second Unit
+            </a>
+          </div>
+        </div>
+
+        <div
+          id="first-section"
+          class={`mt-5 ${activeSection !== "first" ? "d-none" : ""}`}
+        >
+          <First />
+        </div>
+
+        <div
+          id="second-section"
+          class={`mt-5 ${activeSection !== "second" ? "d-none" : ""}`}
+        >
+          <Second />
         </div>
       </div>
-
-      <First id="first" />
     </>
   );
 };
